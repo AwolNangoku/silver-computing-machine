b5766042c3db525e303ad0976381a56e7409a10a
ncedile <ncedile@mcmsolutions.co.uk>
2022-03-03 22:22:56 +0200

    small updates

--- lib/screens/app.dart
+++ lib/screens/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:grandeur_app/models/user.dart';
 import 'package:grandeur_app/screens/bookings.dart';
 import 'package:grandeur_app/screens/login.dart';
 import 'package:grandeur_app/screens/profile.dart';
