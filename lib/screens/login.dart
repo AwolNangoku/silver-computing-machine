6836491646660c4861852329f47a7c49a7824391
ncedile <ncedile@mcmsolutions.co.uk>
2022-03-10 01:31:40 +0200

    adds code that uses Form widget for data submission

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -5,6 +5,20 @@ import 'package:grandeur_app/utils/server_post.dart';
 import 'package:grandeur_app/screens/register.dart';
 import 'package:localstorage/localstorage.dart';
 
+class LoginData {
+  String? username;
+  String? password;
+
+  LoginData({
+    this.username,
+    this.password,
+  });
+
+  Object toJson() {
+    return {'username': username, 'password': password};
+  }
+}
+
 class Login extends StatefulWidget {
   const Login({Key? key, required this.title}) : super(key: key);
 
@@ -18,89 +32,105 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final TextEditingController _username = TextEditingController();
-  final TextEditingController _password = TextEditingController();
-  late bool isSigningUp = false;
+  late bool isSigningIn = false;
+  LoginData loginFormData = LoginData();
+
   LocalStorage storage = LocalStorage('grandeur_app');
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        padding: const EdgeInsets.all(20.0),
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: !isSigningUp
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 8),
-                      child: Expanded(
-                          child: TextField(
-                        controller: _username,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Email Address',
-                        ),
-                      ))),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                    child: Expanded(
-                        child: TextField(
-                      obscureText: true,
-                      controller: _password,
-                      decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Password',
-                      ),
-                    )),
-                  ),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              isSigningUp = true;
-                            });
-                            serverPost('http://10.0.2.2:3000/account/login', {
-                              'username': _username.text,
-                              'password': _password.text
-                            }).then((value) => {
-                                  storage.setItem(
-                                      'user', User.fromJson(value['user'])),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(20),
+          child: !isSigningIn
+              ? Form(
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 8),
+                        child: Expanded(
+                            child: TextFormField(
+                          autofocus: true,
+                          textInputAction: TextInputAction.next,
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            hintText: 'example@domain.com',
+                            labelText: 'Email Address',
+                          ),
+                          onChanged: (value) {
+                            loginFormData.username = value;
+                          },
+                        ))),
+                    Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 8),
+                        child: Expanded(
+                            child: TextFormField(
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            hintText: '****************',
+                            labelText: 'Password',
+                          ),
+                          obscureText: true,
+                          onChanged: (value) {
+                            loginFormData.password = value;
+                          },
+                        ))),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          ElevatedButton(
+                            onPressed: () async {
+                              setState(() {
+                                isSigningIn = true;
+                              });
+                              try {
+                                var loginResults = await serverPost(
+                                    'http://10.0.2.2:3000/account/login',
+                                    loginFormData.toJson());
+
+                                if (loginResults['success']) {
+                                  var loggedInuser =
+                                      User.fromJson(loginResults['user']);
+                                  storage.setItem('user', loggedInuser);
+
                                   Navigator.of(context).pushReplacementNamed(
                                       Profile.routeName,
-                                      arguments: User.fromJson(value['user']))
+                                      arguments: loggedInuser);
+                                } else {
+                                  setState(() {
+                                    isSigningIn = false;
+                                  });
+                                }
+                              } catch (error) {
+                                setState(() {
+                                  isSigningIn = false;
                                 });
-                          },
-                          child: const Text('Sign in'),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, Register.routeName);
-                          },
-                          child: const Text('Sign up'),
-                        )
-                      ])
-                ],
-              )
-            : const CircularProgressIndicator(),
-      ),
-    );
+                              }
+                            },
+                            child: const Text('Sign in'),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, Register.routeName);
+                            },
+                            child: const Text('Sign up'),
+                          )
+                        ])
+                  ],
+                ))
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                      CircularProgressIndicator(),
+                      Text('Signing in...')
+                    ]),
+        ));
   }
 }
